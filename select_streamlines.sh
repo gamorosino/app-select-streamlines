1da2c88be18d32140a44c9a69f218f97c3f7b5ed
Gabriele Amorosino <43575424+gamorosino@users.noreply.github.com>
2025-06-08 18:57:06 -0500

    Update select_streamlines.sh

--- select_streamlines.sh
+++ select_streamlines.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
+
+nthreads_available () {
+            
+                        
+			local cpu_load=$( echo $( top -b -n2 | grep "Cpu(s)" | awk '{print $2+$4}' | tail -n1 ))		
+			local cpu_load_f=$(echo "scale=5; ${cpu_load}/100  " | bc)
+			local cpu_num_all_n=$(( $( getconf _NPROCESSORS_ONLN )-0 ))	
+			local cores_used=$(echo "scale=5; ${cpu_load_f}*${cpu_num_all_n} " | bc)
+			local cores_used=${cores_used%.*}
+			local cores_used=$(( ${cores_used}-0 ))								
+			local cpu_num_all=$(( cpu_num_all_n-$cores_used))							
+			echo $cpu_num_all
+
+			};
+
+# Limit parallel jobs
+max_jobs=$( nthreads_available ) 
+
+wait_for_jobs() {
+    while [ "$(jobs -r | wc -l)" -ge "$max_jobs" ]; do
+        sleep 1
+    done
+}
+
+
 tractogram=$1
 ecc_polar_dir=$2
 ends_only=$3
 outputdir=$4
+
+[ "$ends_only" == "true" ] && ends_only_cmd="--ends_only"
 
 mkdir -p "$outputdir"
 
