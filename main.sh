01dde8c72dd40878403b6aaed75e94567350ac3f
Gabriele Amorosino <43575424+gamorosino@users.noreply.github.com>
2025-06-07 23:16:21 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,25 +1,35 @@
 #! /bin/bash
 
+
+CPUs_available () {
+        
+                        
+			local cpu_load=$( echo $( top -b -n2 | grep "Cpu(s)" | awk '{print $2+$4}' | tail -n1 ))		
+			local cpu_load_f=$(echo "scale=5; ${cpu_load}/100  " | bc)
+			local cpu_num_all_n=$(( $( getconf _NPROCESSORS_ONLN )-0 ))	
+			local cores_used=$(echo "scale=5; ${cpu_load_f}*${cpu_num_all_n} " | bc)
+			local cores_used=${cores_used%.*}
+			local cores_used=$(( ${cores_used}-0 ))								
+			local cpu_num_all=$(( cpu_num_all_n-$cores_used))							
+			echo $cpu_num_all
+
+			};
+
 imm_unpackSeg() {
-    if [ $# -lt 1 ]; then  # Check if at least one argument is provided
+    if [ $# -lt 1 ]; then
         echo "$0: usage: imm_unpackSeg <segmentation.ext> [<output_folder>]"
         return 1
-    fi  
+    fi
 
     local seg=$1
     local ofolder=$2
-
-    # Extract filename without extension
     local base_name=$(basename "$seg" | sed 's/\..*//')
-
-    # If output folder is not provided, set default to <filename>_unpackSeg
-    [ -z "${ofolder}" ] && { 
+    [ -z "${ofolder}" ] && {
         ofolder=$(dirname "$seg")'/'"${base_name}_unpackSeg/"
     }
 
     mkdir -p "$ofolder"
 
-    # Call the Python function
     python3 - <<EOF
 import os
 import nibabel as nib
@@ -28,7 +38,7 @@ import numpy as np
 def unpack_segmentation(segmentation_file, output_folder, base_name):
     os.makedirs(output_folder, exist_ok=True)
     img = nib.load(segmentation_file)
-    data = np.round(img.get_fdata()).astype(int) 
+    data = np.round(img.get_fdata()).astype(int)
     unique_labels = np.unique(data)
     for label in unique_labels:
         if label == 0:
@@ -42,25 +52,37 @@ unpack_segmentation("$seg", "$ofolder", "$base_name")
 EOF
 }
 
+# Controls how many jobs run in parallel
+max_jobs=$( CPUs_available )
+
+wait_for_jobs() {
+    while (( $(jobs -r | wc -l) >= max_jobs )); do
+        sleep 1
+    done
+}
 
 tractogram=$1
 parc=$2
 ends_only=$3
 outputdir=$4
 
-[ "${ends_only}"  == "true" ] && { ends_only=" --nds_only "; }
+[ "${ends_only}" == "true" ] && ends_only_cmd="--ends_only"
 
 ecc_polar_dir=./ecc_polar
-mkdir ${ecc_polar_dir}
+mkdir -p "${ecc_polar_dir}"
 
-imm_unpackSeg $parc ${ecc_polar_dir}
-mkdir ${outputdir}
+imm_unpackSeg "$parc" "$ecc_polar_dir"
+mkdir -p "${outputdir}"
+
 count=0
 
-for parcel in $( ls ${ecc_polar_dir}/* ); do
+for parcel in "${ecc_polar_dir}"/*; do
+    count=$((count + 1))
+    tck="track_${count}.tck"
 
-  count=$(( $count + 1 ))
-  tck=track_${count}.tck
-  tckedit ${tractogram} -include ${parcel} ${ends_only_cmd} ${outputdir}/${tck}
-  
+    wait_for_jobs
+
+    tckedit "${tractogram}" -include "${parcel}" ${ends_only_cmd} "${outputdir}/${tck}" &
 done
+
+wait  # Wait for all background jobs to finish
