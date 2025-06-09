92756ccbe03f923a08f38db4a25e973d24f162b8
Gabriele Amorosino <43575424+gamorosino@users.noreply.github.com>
2025-06-08 19:03:11 -0500

    Update select_streamlines.sh

--- select_streamlines.sh
+++ select_streamlines.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
 
-nthreads_available () {
-            
-                        
-			local cpu_load=$( echo $( top -b -n2 | grep "Cpu(s)" | awk '{print $2+$4}' | tail -n1 ))		
-			local cpu_load_f=$(echo "scale=5; ${cpu_load}/100  " | bc)
-			local cpu_num_all_n=$(( $( getconf _NPROCESSORS_ONLN )-0 ))	
-			local cores_used=$(echo "scale=5; ${cpu_load_f}*${cpu_num_all_n} " | bc)
-			local cores_used=${cores_used%.*}
-			local cores_used=$(( ${cores_used}-0 ))								
-			local cpu_num_all=$(( cpu_num_all_n-$cores_used))							
-			echo $cpu_num_all
-
-			};
-
-# Limit parallel jobs
-max_jobs=$( nthreads_available ) 
 
 wait_for_jobs() {
+	local max_job=$1
     while [ "$(jobs -r | wc -l)" -ge "$max_jobs" ]; do
         sleep 1
     done
@@ -29,6 +14,7 @@ tractogram=$1
 ecc_polar_dir=$2
 ends_only=$3
 outputdir=$4
+max_jobs=$5
 
 [ "$ends_only" == "true" ] && ends_only_cmd="--ends_only"
 
@@ -40,7 +26,7 @@ for parcel in "$ecc_polar_dir"/*.nii.gz; do
     count=$((count + 1))
     tck="track_${count}.tck"
 
-    wait_for_jobs
+    wait_for_jobs ${max_jobs}
 
     tckedit "$tractogram" -include "$parcel" ${ends_only_cmd} "$outputdir/$tck" &
 done
